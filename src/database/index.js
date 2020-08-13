2e3b2ee3bca2a202a2a8aa78f09659f6306d1c05
David Cardoso <david.lima.cd@gmail.com>
2020-08-13 08:34:56 -0300

    Insert Database Class, using mongoDB

---
+++ src/database/index.js
@@ -0,0 +1,21 @@
+import mongoose from 'mongoose'
+
+class Database{
+    constructor(){
+        this.mongoDatabase()
+        //You can insert more database types
+    }
+
+    mongoDatabase(){
+        mongoose.connect('mongodb://localhost/mongo_database',{
+            useNewUrlParser: true,
+            useUnifiedTopology: true
+        }).then(()=>{
+            console.log('Conection in the mongoDB with success!')
+        }).catch((erro)=>{
+            console.log('Error: '+ erro)
+        })
+    }
+}
+
+export default new Database()
