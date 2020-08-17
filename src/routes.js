861812116c50888cdfe0face336d89c5b940faf3
David Cardoso <david.lima.cd@gmail.com>
2020-08-17 07:51:57 -0300

    Exportado user controllers para rotas

--- src/routes.js
+++ src/routes.js
@@ -1,5 +1,6 @@
 import { Router } from 'express'
 import User from './app/models/User'
+import UserController from './app/controllers/UserController'
 
 const routes = new Router()
 
@@ -11,17 +12,6 @@ routes.get('/user-list',(req,res)=>{
     res.send(['David','Jack','Lulu'])
 })
 
-routes.get('/user-insert', async (req, res)=>{
-    await User.create({
-        name: 'Ana',
-        email: 'ana@dav.com',
-        password: '123'
-    }, function(err,small){
-        if(err)
-            return res.status(400).json({error:"User can't be created: "+ err})
-
-        return res.status(200).json({error:"User create with success!"})
-    })
-})
+routes.post('/user-insert',UserController.store)
 
 export default routes
