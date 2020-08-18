3316c5008a62abf19424bf6a085285117e60c88a
David Cardoso <david.lima.cd@gmail.com>
2020-08-18 08:52:04 -0300

    Alterado validações Yup, percorrendo erros e erros customizados

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,46 +1,54 @@
-import * as yup from 'yup';
+import * as Yup from 'yup';
 import User from '../models/User'
 
 class UserController{
     //Insert User
     async store(req, res){
 
-        const schema = yup.object().shape({
-            name: yup.string().required(),
-            email: yup.string().email().required(),
-            name: yup.string().required().min(6)
-        })
+        try{
+            //Yup validation data
+            const schema = Yup.object().shape({
+                name: Yup.string().required('Name is required!'),
+                email: Yup.string().email().required('E-mail is required!'),
+                password: Yup.string().required('Name is required!').min(6,'Password min 6 characters!')
+            })
+            await schema.validate(req.body, {
+                abortEarly: false
+            })
 
-        if(!(await schema.isValid(req.body))){
-            return res.status(400).json({
+            //Check email user is already exists
+            const checkEmailExists = await User.findOne({email: req.body.email})
+            if(checkEmailExists) return res.status(400).json({
                 error: true,
                 code: 102,
-                message: "Invalid date"
-            })
-        }
-
-        //Check email user is already exists
-        const checkEmailExists = await User.findOne({email: req.body.email})
-        if(checkEmailExists) return res.status(400).json({
-            error: true,
-            code: 102,
-            message: "User email already exists!"
-        })
-
-        const user = await User.create(req.body,(err)=>{
-
-            if(err) return res.status(400).json({
-                error:true,
-                code: 101,
-                message: "Erro to insert user: " + err
+                message: "User email already exists!"
             })
 
-            return res.status(200).json({
-                error: false,
-                message: "Insert user with success!",
-                data: user
+            const user = await User.create(req.body,(err)=>{
+                return res.status(200).json({
+                    error: false,
+                    message: "Insert user with success!",
+                    data: user
+                })
             })
-        })
+
+        }catch(err){
+
+            //Take only Yup error
+            if(err instanceof Yup.ValidationError){
+                const errorMessages = {}
+                //Capture errors
+                err.inner.forEach(error =>{
+                    errorMessages[error.path] = error.message
+                })
+
+                return res.status(400).json({
+                    error:true,
+                    code: 101,
+                    message: errorMessages
+                })
+            }
+        }s
     }
 }
 
